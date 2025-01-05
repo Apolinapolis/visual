3ad89bbf2815e59d0c51c59e7f3fbb85a6e3f402
Apolinapolis <larra12@ya.ru>
2025-01-05 17:11:17 +0300

    50_000 points fullScreen

--- rw_visual.py
+++ rw_visual.py
@@ -5,15 +5,27 @@ from random_walk import RandomWalk
 while True:
 
     # Постороение случайного блуждания
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     #Нанесение точек на диаграмму
     plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, s=15)
+    fig, ax = plt.subplots(figsize = (15,9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors='none', s=1)
+    
+    # Выделение первого и последнего точек
+    ax.scatter(0, 0, c='green', s=100, edgecolors='none')
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
+               s=100, edgecolors='none')
+    
+    # Удаление осей
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
-    keep_running = input("Хотите ли вы продолжить? (да/нет): ")
-    if keep_running == 'нет':
+    keep_running = input("Хотите ли вы продолжить? (y/n): ")
+    if keep_running == 'n':
         break
